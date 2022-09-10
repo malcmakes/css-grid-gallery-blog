dcb05ae7c58f18ffdd999581a5e9fd8ab65f1440
Malcolm <malcolmharrellmedia@gmail.com>
2022-09-10 15:38:35 -0700

    files added for better structure

--- js/center.js
+++ js/center.js
@@ -3,6 +3,7 @@
 
 // Naviator to different grid by pressing buttons
 
+// Function created to insert imgs into css grid boxes
 function createImg (name, URL, boxName, width) {
 // Create BLANK img object so HTML box is able to hold IMG
 name = document.createElement("img")
@@ -14,12 +15,10 @@ name.style.width = `${width}%`
 document.querySelector(`.${boxName}`).appendChild(name)
 }
 
-createImg('img1','./movie-imgs/airbud_.jpg','box1', 30)
-createImg('img2','./movie-imgs/holes.jpg','box2', 80)
-createImg('img3','./movie-imgs/blade.jpg','box3', 80)
-createImg('img4','./movie-imgs/spykids.jpg','box4', 80)
-createImg('img5','./movie-imgs/outcold.jpg','box5', 80)
-createImg('img6','./movie-imgs/punks_.jpg','box6', 80)
-createImg('img7','./movie-imgs/theotherme.jpg','box7', 80)
-// create ('img3','./movie-img/')
-
+createImg('img1','../movie-imgs/airbud_.jpg','box1')
+createImg('img2','../movie-imgs/holes.jpg','box2')
+createImg('img3','../movie-imgs/blade.jpg','box3')
+createImg('img4','../movie-imgs/spykids.jpg','box4')
+createImg('img5','../movie-imgs/outcold.jpg','box5')
+createImg('img6','../movie-imgs/punks_.jpg','box6')
+createImg('img7','../movie-imgs/theotherme.jpg','box7')
