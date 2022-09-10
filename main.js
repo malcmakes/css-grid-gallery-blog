82ec89c7d29d578534fc3faee87b86ecfce40722
Malcolm <malcolmharrellmedia@gmail.com>
2022-09-10 14:12:22 -0700

    missing imgs fixed

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ name.style.width = `${width}%`
 document.querySelector(`.${boxName}`).appendChild(name)
 }
 
-createImg('img1','./movie-img/airbud_.jpg','box1', 30)
-createImg('img2','./movie-img/holes.jpg','box2', 80)
-createImg('img3','./movie-img/blade.jpg','box3', 80)
-createImg('img4','./movie-img/spykids.jpg','box4', 80)
-createImg('img5','./movie-img/outcold.jpg','box5', 80)
-createImg('img6','./movie-img/punks_.jpg','box6', 80)
-createImg('img7','./movie-img/theotherme.jpg','box7', 80)
+createImg('img1','./movie-imgs/airbud_.jpg','box1', 30)
+createImg('img2','./movie-imgs/holes.jpg','box2', 80)
+createImg('img3','./movie-imgs/blade.jpg','box3', 80)
+createImg('img4','./movie-imgs/spykids.jpg','box4', 80)
+createImg('img5','./movie-imgs/outcold.jpg','box5', 80)
+createImg('img6','./movie-imgs/punks_.jpg','box6', 80)
+createImg('img7','./movie-imgs/theotherme.jpg','box7', 80)
 // create ('img3','./movie-img/')
 
